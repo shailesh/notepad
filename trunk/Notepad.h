8125729a7663e3de5f8c1981e56551b917fa8b34
wildwoong@gmail.com <wildwoong@gmail.com@a404485a-1631-683b-f420-bf854b3e9597>
2011-01-21 18:10:53 +0000

    D:\Notepad\Notepad.h

---
+++ trunk/Notepad.h
@@ -0,0 +1,13 @@
+// Notepad.h
+
+#ifndef _NOTEPAD_H
+#define _NOTEPAD_H
+
+#include <afxwin.h>
+
+class CNotepadApp:public CWinApp{
+public:
+	virtual BOOL InitInstance();
+};
+
+#endif //_NOTEPAD_H
